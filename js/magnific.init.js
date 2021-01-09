5609a5698a6abb7424643b96c5e889c5379de074
Bern <bernnystrom@gmail.com>
2021-01-09 15:59:18 +0800

    Initial commit of completed portfolio

---
+++ js/magnific.init.js
@@ -0,0 +1,12 @@
+//Cta Video
+$('.mfp-image').magnificPopup({
+    disableOn: 375,
+    type: 'image',
+    mainClass: 'mfp-fade',
+    removalDelay: 160,
+    preloader: false,
+    fixedContentPos: false,
+    gallery: {
+      enabled: true
+    }
+});
